0792eb94362204b87dc63e64730644520ac470b7
Kristian Willey <kristianwilley@gmail.com>
2024-02-03 23:18:17 -0800

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,34 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import MovieList from './MovieList';
+import MovieListHeading from './MovieListHeading';
+import SearchBox from './SearchBox';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import AddFavorites from './AddFavorites'
+
 const App = () => {
-  //const[movies, setMovies] = useState
+  const[movies, setMovies] = useState([]);
+  const [favorites, setFavorites] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
   const num = 4; 
-  const[movies, setMovies] = useState([
+
+  const getMovieRequest = async (searchValue) => {
+    //setSearchValue
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=f4fb61e0`;
+    const response = await fetch(url);
+    const jsonResponse = await response.json(); // converts http to json
+
+    console.log(jsonResponse);
+
+    if(jsonResponse.Search){
+        setMovies(jsonResponse.Search);
+    }
+}
+
+useEffect(()=>{
+    getMovieRequest(searchValue);
+},[searchValue]); 
+  /*const[movies, setMovies] = useState([
     {
       "Title": "Spy Kids",
       "Year": "2001",
@@ -75,24 +99,39 @@ const App = () => {
       "Type": "movie",
       "Poster": "N/A"
   }
-  ]);
+  ]); 
+    <div className="container-fluid movie-app">
+        <div className="row d-flex align-items-center mt-4 mb-4">
+         <MovieListHeading heading =  'Movies' />
+         <SearchBox searchValue = {searchValue} setSearchValue={setSearchValue}/>
+        </div>   */
 
+  // {Title, Year, imdbID, Type, Poster}
   return (
-    <div className="container-fluid">
-        <div className="row">
-            <MovieList mov = {movies} />
+      
+    
+        <div className="container-fluid movieapp">
+
+            <div className = "row d-flex align items center mt-4 mb-4">
+                <MovieListHeading heading = "Movies" />
+                <SearchBox searchValue = {searchValue} setSearchValue = {setSearchValue} />
+            </div>
+
+            <div className="row">
+                    <MovieList mov = {movies}/>   
+            </div>
         </div> 
 
-    </div>
   );
-  /*
-  const getMovieRequest = async () => {
-    const url = 'http://www.omdbapi.com/?s=fast five&apikey=f4fb61e0';
-    const response = await fetch(url);
-    const jsonResponse = await response.json();
+  
 
-    console.log(jsonResponse);
-  }*/
+
+
+
+
+  
 }
 
 export default App;
+
+
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -4,9 +4,11 @@ const MovieList = (props) => {
 // mov is an object of MovieList
     return (
         <>
-            {props.mov.map( (movie, index) => <div>
-                <img src = {movie.Poster} alt="Poster"></img>
-            </div>)}
+            {props.mov.map( (movie, index) =>( 
+            <div className='image-container d-flex justify-content-start m-3'>
+                <img src = {movie.Poster} alt="movie"></img>
+            </div>
+            ))}
         </>
     )
 }
